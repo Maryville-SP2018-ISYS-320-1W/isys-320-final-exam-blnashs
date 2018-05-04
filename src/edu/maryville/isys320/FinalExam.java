fbd2e7de39496db9164727fca9177b22297e85e0
Barbara Nash <bnash1@live.maryville.edu>
2018-05-04 16:29:27 -0500

    string 1

--- src/edu/maryville/isys320/FinalExam.java
+++ src/edu/maryville/isys320/FinalExam.java
@@ -1,15 +1,99 @@
 package edu.maryville.isys320;
 
+import java.util.Scanner;
+
 /*
  * Class:    ISYS-320
- * Student:  <REPLACE WITH YOUR NAME>
+ * Student:  Barb Nash
  * Date:     30-APR-2018
  */
 
 public class FinalExam {
 
 	public static void main(String[] args) {
+		double a, double b;
+		double c = Math.sqrt(Math.pow(a,  2) + Math.pow(b,  2));
+		return Math.sqrt(Math.pow(a, 2) + Math.pow(b,  2));
+		System.out.println(Math.sqrt(a);
+		
+		
 		
 	}
 
 }
+
+public static void main class DistanceFinder(String[] args
+private int value;) {
+int totalSum = sumTo( 100 );
+System.out.println("The distance is " + totalSum );
+
+}
+
+ int sumTo(int n) { 
+for (int i = 1; i <= n; i++) {
+    int sum = 0; 
+     sum += };
+    int sumTo(int x) { 
+    	for (int i = 1; i <= x; i++) {
+    	    int sum = 0; 
+    	    sum += i;
+}
+return value;
+}
+
+}
+ 
+ public class TriangleWord {
+
+		public static void main(String[] args) {
+			for(line < 11; line-- ) {
+				int = line % 5 + 1 ;
+				writeChar("Please enter a word up to eleven characters");
+				System.out.println();
+			}
+
+		}
+		
+		public static void writeChar( char characterToPrint numOfTimesPrinted) {
+			for( int numOfTimesPrinted = 1; numOfTimesPrinted <= numOfTimesToPrint; numOfTimesPrinted++ ) {
+				System.out.print( characterToPrint );
+			}
+		}
+	}
+ 
+ public class PointBetweenTwoObjects {
+	 int x;
+	 int y;
+	 //returns the distance between this point and (0, 0)
+	 return Math.sqrt(x * x + y * y);
+ }//shifts this point's location by the given amount
+ public void translate(int dx, int dy) {
+	 x += dx;
+	 y += dy;
+ }
+ }
+ System.out.println("distance from origin = " + pl.distanceFromOrigin();
+ pl.translate(11, 6);
+ 
+ }
+ 
+ public class golfScore {
+
+		public static void main(String[] args) {
+			Scanner console = new Scanner(System.in);
+			System.out.print("What is par for the course?");;
+			int numPar = console.nextInt();
+			int sum = 0;
+			for {int i = 1; i <= numPar; i++) {
+	 System.out.print("Enter your next round score:");
+	 int next = concole.nextInt();
+	 sum += next;
+ }
+ double average = (double) sum / numPar;
+ System.out.println(x);;
+ System.out.println("Average = " + average);;
+			}
+		}
+		
+ }
+ 
